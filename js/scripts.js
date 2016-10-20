0aba7152ff9b412d81fe0aa292780a4bfcca90e9
zb1334802314 <mei_shen_zb@sina.com>
2016-10-20 16:19:29 +0800

    html

---
+++ js/scripts.js
@@ -0,0 +1,158 @@
+
+(function() {
+    "use strict";
+
+    // custom scrollbar
+
+    $("html").niceScroll({styler:"fb",cursorcolor:"#65cea7", cursorwidth: '6', cursorborderradius: '0px', background: '#424f63', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
+
+    $(".left-side").niceScroll({styler:"fb",cursorcolor:"#65cea7", cursorwidth: '3', cursorborderradius: '0px', background: '#424f63', spacebarenabled:false, cursorborder: '0'});
+
+
+    $(".left-side").getNiceScroll();
+    if ($('body').hasClass('left-side-collapsed')) {
+        $(".left-side").getNiceScroll().hide();
+    }
+
+
+
+    // Toggle Left Menu
+    // 二级菜单被点击
+   jQuery('.menu-list > a').click(function() {
+      
+      var parent = jQuery(this).parent();
+      var sub = parent.find('> ul');
+      
+      if(!jQuery('body').hasClass('left-side-collapsed')) {
+         if(sub.is(':visible')) {
+            sub.slideUp(200, function(){
+               parent.removeClass('nav-active');
+               jQuery('.main-content').css({height: ''});
+               mainContentHeightAdjust();
+            });
+         } else {
+            visibleSubMenuClose();
+            parent.addClass('nav-active');
+            sub.slideDown(200, function(){
+                mainContentHeightAdjust();
+            });
+         }
+      }
+      return false;
+   });
+
+   //一级菜单被点击
+   jQuery('.first > a').click(function() {
+      //清除所有li的active属性
+      $("li").removeClass('active');
+      //查找有没有打开的二级菜单，有，收起来
+      jQuery('.menu-list').each(function() {
+         var t = jQuery(this);
+         //如果他是展开的，那么收起来，并且查看他的子选项是否有active属性
+         if(t.hasClass('nav-active')) {
+            //收起来
+            t.find('> ul').slideUp(200, function(){
+               t.removeClass('nav-active');
+            });
+         }
+      });
+      //得到它自己，将他的active属性激活
+      var self = jQuery(this).parent();
+      //如果它没有active属性，添加该属性
+      if (!self.hasClass('active')) {
+          self.addClass('active');
+      }
+   });
+
+   //二级菜单被点击
+   jQuery('.sub-menu-list a').click(function() {
+      //清除所有li的active属性
+      $("li").removeClass('active');
+      //激活他自己
+      $(this).parent().addClass('active');
+    
+   });
+   
+   function visibleSubMenuClose() {
+      jQuery('.menu-list').each(function() {
+         var t = jQuery(this);
+         if(t.hasClass('nav-active')) {
+            t.find('> ul').slideUp(200, function(){
+               t.removeClass('nav-active');
+            });
+         }
+      });
+   }
+
+   function mainContentHeightAdjust() {
+      // Adjust main content height
+      var docHeight = jQuery(document).height();
+      if(docHeight > jQuery('.main-content').height())
+         jQuery('.main-content').height(docHeight);
+   }
+
+   //  class add mouse hover
+   jQuery('.custom-nav > li').hover(function(){
+      jQuery(this).addClass('nav-hover');
+   }, function(){
+      jQuery(this).removeClass('nav-hover');
+   });
+
+
+   // Menu Toggle
+   jQuery('.toggle-btn').click(function(){
+       $(".left-side").getNiceScroll().hide();
+       
+       if ($('body').hasClass('left-side-collapsed')) {
+           $(".left-side").getNiceScroll().hide();
+       }
+      var body = jQuery('body');
+      var bodyposition = body.css('position');
+
+      if(bodyposition != 'relative') {
+
+         if(!body.hasClass('left-side-collapsed')) {
+            body.addClass('left-side-collapsed');
+            jQuery('.custom-nav ul').attr('style','');
+
+            jQuery(this).addClass('menu-collapsed');
+
+         } else {
+            body.removeClass('left-side-collapsed chat-view');
+            jQuery('.custom-nav li.active ul').css({display: 'block'});
+
+            jQuery(this).removeClass('menu-collapsed');
+
+         }
+      } else {
+
+         if(body.hasClass('left-side-show'))
+            body.removeClass('left-side-show');
+         else
+            body.addClass('left-side-show');
+
+         mainContentHeightAdjust();
+      }
+
+   });
+   
+
+  
+
+   jQuery(window).resize(function(){
+
+      if(jQuery('body').css('position') == 'relative') {
+
+         jQuery('body').removeClass('left-side-collapsed');
+
+      } else {
+
+         jQuery('body').css({left: '', marginRight: ''});
+      }
+
+
+
+   });
+
+
+})(jQuery);
